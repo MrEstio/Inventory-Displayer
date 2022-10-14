5028c5b975a955a462a3e6768f945518c504927f
Deku <Deku@PREDATOR>
2022-10-14 21:08:06 +0300

    Добавьте файлы проекта.

---
+++ Inventory Displayer/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+void display(const vector<string>& inventory);
+int main()
+{
+	vector<string> inventory;
+	inventory.push_back("sword");
+	inventory.push_back("armour");
+	inventory.push_back("shield");
+	display(inventory);
+	return 0;
+}
+void display(const vector<string>& vec)
+{
+	cout << "Your items:\n";
+	for (vector<string>::const_iterator iter = vec.begin();
+		iter != vec.end(); ++iter)
+	{
+		cout << *iter << endl;
+	}
+
+}
